4780ef3d4208c0a8582320b8d4e65af2b56cefbd
David030822 <demeter.david0322@gmail.com>
2023-03-12 18:09:04 +0200

    Labor 1

--- labor-01/src/oop/labor01/Main.java
+++ labor-01/src/oop/labor01/Main.java
@@ -3,5 +3,26 @@ package oop.labor01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("labor01");
+
+        String name = "David Demeter";
+        String country = "Romania";
+        System.out.println(name + ", " + country);
+
+        int n = name.length();
+        for (int i = 0; i < n; i++) {
+            System.out.println(name.charAt(i));
+        }
+
+        String name2 = "Joe Donald Biden";
+        String [] arrOfString = name2.split(" ");
+        for (String a : arrOfString)
+            System.out.println(a);
+
+        String name3 = "TREE";
+        n = name3.length();
+        for (int i = 0; i < n; i++) {
+            System.out.println(name3.substring(0, i + 1));
+        }
+
     }
 }
