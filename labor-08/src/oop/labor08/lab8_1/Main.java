12a2df27ad777a8aaaf5e7fc0267f1c0650577ce
David030822 <demeter.david0322@gmail.com>
2023-04-22 13:17:52 +0300

    Labor8_1

--- labor-08/src/oop/labor08/lab8_1/Main.java
+++ labor-08/src/oop/labor08/lab8_1/Main.java
@@ -1,7 +1,9 @@
-package oop.labor08;
+package oop.labor08.lab8_1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("labor08");
+
+        System.out.println("labor08_1");
+
     }
 }
---
+++ labor-08/src/oop/labor08/lab8_1/SavingsAccount.java
@@ -0,0 +1,22 @@
+package oop.labor08.lab8_1;
+
+public class SavingsAccount extends oop.labor08.lab8_1.BankAccount {
+    private double interestRate;
+
+    public SavingsAccount(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void addInterest(){
+        this.balance += this.interestRate * this.balance;
+    }
+
+}
