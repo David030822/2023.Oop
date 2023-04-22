caeb9eb534795ee8feb0abd09914f992a1b63f21
David030822 <demeter.david0322@gmail.com>
2023-04-22 13:18:13 +0300

    Labor8_2

---
+++ labor-08/src/oop/labor08/lab8_2/Main.java
@@ -0,0 +1,9 @@
+package oop.labor08.lab8_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("labor08_2");
+
+    }
+}
---
+++ labor-08/src/oop/labor08/lab8_2/StackInheritance.java
@@ -0,0 +1,5 @@
+package oop.labor08.lab8_2;
+
+public class StackInheritance {
+
+}
