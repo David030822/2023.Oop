ce530f1b15239c4c69389706567a6b00f72af024
David030822 <demeter.david0322@gmail.com>
2023-05-19 20:56:57 +0300

    Labor6_1

--- labor-06/src/oop/labor06/lab6_1/Customer.java
+++ labor-06/src/oop/labor06/lab6_1/Customer.java
@@ -3,30 +3,45 @@ package oop.labor06.lab6_1;
 import java.util.ArrayList;
 
 public class Customer {
+    private int numCustomers;
     private final String firstName;
     private String lastName;
-    private final int id = 1;
+    private int id = 0;
     private final ArrayList<BankAccount> accounts = new ArrayList<>();
 
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        ++id;
     }
 
     public int getId() {
         return id;
     }
 
+    public int getNumAccounts(){
+        return accounts.size();
+    }
+
     public void addAccount(BankAccount account) {
         accounts.add(account);
     }
 
-    public BankAccount getAccountNumbers(String accountNumber) {
-        for (BankAccount account:accounts) {
-            if(account.getAccountNumber().equals(accountNumber))
+    public BankAccount getAccount(String accountNumber){
+        for (BankAccount account:accounts){
+            if(account.getAccountNumber().equals(accountNumber)){
                 return account;
+            }
         }
         return null;
+    }
+
+    public ArrayList<String> getAccountNumbers() {
+        ArrayList<String> accountNumbers = new ArrayList<>();
+        for (BankAccount account:accounts) {
+            accountNumbers.add(account.getAccountNumber());
+        }
+        return accountNumbers;
     }
 
     public String getFirstName() {
@@ -48,7 +63,7 @@ public class Customer {
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        result.append(firstName).append(' ').append(lastName).append(" accounts:\n");
+        result.append(firstName).append(' ').append(lastName).append(' ').append(id).append(" accounts:\n");
         for (BankAccount account:accounts) {
             result.append("\t").append(account).append("\n");
         }
